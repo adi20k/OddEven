f943e3e7712f101f52f913ccdd7c644ed052da19
Aditya Raghunath Khajindar <43776909+adi20k@users.noreply.github.com>
2021-10-17 21:48:49 +0530

    Add files via upload

---
+++ src/oddeven/OddEven.java
@@ -0,0 +1,16 @@
+package oddeven;
+import java.util.*;
+public class OddEven {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the number");
+		int num=sc.nextInt();
+		if(num%2==0)
+			System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+
+	}
+
+}
